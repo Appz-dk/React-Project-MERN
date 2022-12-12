03233b7ce8053fe9346ae24e12966ed4994335d3
Appz-dk <patrick.fyhn@gmail.com>
2022-12-12 23:02:15 +0100

    Added GET route to /decks to get all decks from database

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response } from "express"
-import mongoose from 'mongoose';
+import mongoose, { mongo } from 'mongoose';
 import cors from "cors"
 
 import * as dotenv from 'dotenv' 
@@ -12,13 +12,20 @@ const PORT = 5000
 const app = express()
 
 const corsOptions = {
-    origin: 'http://localhost:5173',
+    origin: ["http://localhost:5173", "https://www.thunderclient.com"],
     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
   }
 
 // Use of middleware functions
 app.use(express.json())
 app.use(cors(corsOptions))
+
+app.get("/decks", async (req: Request, res: Response) => {
+    // Get all decks from database
+    const decks = await Deck.find()
+    // Send all decks back to UI
+    res.json(decks)
+})
 
 app.post("/decks", async (req: Request, res: Response) => {
     // const {title} = req.body
